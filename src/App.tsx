56ca54826dce6b2adb027553457abf901d03ee7c
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-21 10:23:16 -0400

    jsx setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,27 @@ function App() {
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
-  const [secounds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    const target = new Date('12/21/2022 23:59:59')
+    const target = new Date('21/03/2022 10:24:59');
     const interval = setInterval(() => {
-      const now = new Date()
+      const now = new Date();
 
-      const difference = target.getTime() - now.getTime()
-      const d = Math.floor(difference/(1000 * 60 *60 * 24))
-      setDays(d)
+      const difference = target.getTime() - now.getTime();
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
 
-      const h = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 *60))
-      setHours(h)
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
 
-      const m = Math.floor((difference % (1000 * 60 * 60 )) / (1000 *60))
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
 
-      setMinutes(m)
-      const s = Math.floor((difference % (1000 * 60)) /1000)
-      setSeconds(s)
-
+      setMinutes(m);
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
     }, 1000);
 
     return () => {
@@ -34,7 +35,26 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='timer-wrapper'>DAYS {days}</div>
+      <div className='timer-wrapper'>
+        <div className='timer-inner'>
+          <div className='timer-segment'>
+            <span className='time'>{days}</span>
+            <span className='label'>Days</span>
+          </div>
+          <div className='timer-segment'>
+            <span className='time'>{hours}</span>
+            <span className='label'>Hours</span>
+          </div>
+          <div className='timer-segment'>
+            <span className='time'>{minutes}</span>
+            <span className='label'>Minutes</span>
+          </div>
+          <div className='timer-segment'>
+            <span className='time'>{seconds}</span>
+            <span className='label'>Seconds</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
